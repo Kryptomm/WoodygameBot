a3659e3064150fdef775286b925898396692c264
Kryptom <tom11032002@gmail.com>
2023-07-28 21:26:13 +0200

    Commit Farming

--- test.py
+++
@@ -1,9 +0,0 @@
-txt = """
-    E , M , C , C , M , P , C , C , M , P , P , T , T , M , P , T , E , P , E , P ,
-    E , P , E , C , M , C , P , M , M , C , P , E , P , M , C , P , E , T , T , P ,
-    E , P , E , C , M , C , T , T , M , C , C , T , P , M , P , C , E , E , M , P ,
-    E , T , P , C , M , C , P , P , T , C , P , E , T , T , P , P , E , E , T , P ,
-    E , M , T , C , M , C , P , E , C , C , P , E , P , T , P , P , E , P , E , P .
-    """
-
-print(txt.count("T"))
